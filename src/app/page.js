8db0a4f83c7d7655b533838863580e38156bcb27
kangChangputh <126743826+kangChangputh@users.noreply.github.com>
2024-10-24 17:41:55 +0700

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,36 @@
+
+"use client"
+import { useState, useEffect } from "react";
+import Form from "@/components/Form";
+import Header from "@/components/Header";
+import TODOHero from "@/components/TODOHero";
+import TODOList from "@/components/TODOList";
+
+function Home() {
+  const [todos, setTodos] = useState([]);
+  
+  //Retrieve data from localStorage when components mounts
+  useEffect(() => {
+    const storedTodos = localStorage.getItem("todos");
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos))
+    }
+  }, [])
+
+const todos_completed = todos?.filter(
+    (todo) => todo.is_completed === true 
+  ).length
+  
+  const total_todos = todos.length
+  return (
+    <div className="wrapper">
+      <Header />
+      <TODOHero todos_completed={todos_completed} total_todos={total_todos}/>
+      <Form todos={todos}  setTodos={setTodos} />
+      <TODOList todos={todos} setTodos={setTodos}/>
+    </div>
+  );
+}
+export default Home;
+
+
